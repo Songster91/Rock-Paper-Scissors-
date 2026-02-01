bcf882e95197c02baa3cb257d238b0f1f8661082
songster <192950245+Songster91@users.noreply.github.com>
2026-02-01 13:18:54 +0600

    successfully complete the first step which is get a random choice from the computer as the game is played against the computer i create a function here and inside the function i create a variable with math.random and then apply the if else statement to gain the wanted result

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 # Rock-Paper-Scissors
-this is my frist js project of Javascript for the top.
-i have learned a bunch of things so far and im trying to impliment the thing here. but as i do remember thing well so i need to look back to the previous lessons which is totally file.
+this is my first js project of Javascript for the top.
+i have learned a bunch of things so far and im trying to implement the thing here. but as i do remember thing well so i need to look back to the previous lessons which is totally file.
 lets start with the top instructions---
--- index.js
+++ index.js
@@ -1,2 +1,24 @@
 console.log("Hello world");
 
+// the game is played against the computer so i have to write a function that
+// returns rock paper scissors randomly
+
+//create a function
+//write a code that returns the 3 value randomly
+// we can use the Math.random method here
+
+function getComputerChoice() {
+  let choice = Math.random();
+  if (choice >= 0.6666) {
+    console.log("rock");
+
+
+  } else if (choice >= 0.3333 && choice <= 0.6666) {
+    console.log("paper");
+
+
+  } else{
+    console.log("scissor");
+  }
+}
+console.log(getComputerChoice());
