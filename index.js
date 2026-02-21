bde48576e85ac7389e749d7d03c67384dc491ecb
songster <192950245+Songster91@users.noreply.github.com>
2026-02-21 22:30:14 +0600

    get the human choice after giving some time i am able to fix my code with the help of copilot which is not a good approach but i am not able to find the error in my code so i have to do it. finally i teach myself console log in the function is for show the out put on the console but not for applying the function which is the main problem. and also learn how to call the function and use of return value

--- index.js
+++ index.js
@@ -9,16 +9,33 @@ console.log("Hello world");
 
 function getComputerChoice() {
   let choice = Math.random();
-  if (choice >= 0.6666) {
-    console.log("rock");
-
-
-  } else if (choice >= 0.3333 && choice <= 0.6666) {
-    console.log("paper");
-
-
-  } else{
-    console.log("scissor");
+  if (choice < 0.3333) {
+    return "rock";
+  } else if (choice < 0.6666) {
+    return "paper";
+  } else {
+    return "scissors";
   }
 }
-console.log(getComputerChoice());
+//Now we need to get the human choice .
+// we can use the prompt function here to get the input from the user
+// assume the user always gives the valid input
+
+function getHumanChoice() {
+  return prompt("Please Type Rock, Paper Or scissors");
+  // or
+  // let choice = prompt("Please Type Rock, Paper Or scissors");
+  // return choice;
+
+  
+}
+
+const humanChoice = getHumanChoice();
+const computerChoice = getComputerChoice();
+
+console.log(humanChoice);
+console.log(computerChoice );
+
+
+
+
